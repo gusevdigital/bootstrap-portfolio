8c8148ffac747f8e80c70fc2ff7cb7b9f12ce098
gusevdigital <71307492+gusevdigital@users.noreply.github.com>
2020-09-16 11:10:21 +0700

    Update header.php

--- header.php
+++ header.php
@@ -34,7 +34,7 @@
 		<nav class="navbar navbar-expand-lg navbar-dark ">
 			<div class="container-fluid">
 				<a class="navbar-brand" href="<?php echo esc_url( home_url( '/' ) ); ?>">
-          <?php bloginfo ('name'); ?>
+          <?php bloginfo ('name'); ?>DELETE THIS
         </a>
 				<button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
 					<span class="navbar-toggler-icon"></span>
